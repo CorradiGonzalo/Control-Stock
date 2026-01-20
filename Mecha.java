9edc0c7158194a2b29a0544e9bf0d98873950541
CorradiGonzalo <corradigonzalo9@gmail.com>
2026-01-20 17:48:02 -0300

    Agregamos Mecha.java

---
+++ Mecha.java
@@ -0,0 +1,18 @@
+public class Mecha extends Herramienta {
+    private String material;
+    private int anguloPunta;
+
+    public Mecha(String codigo, double precioBase, int stock, String material, int anguloPunta) {
+         super (codigo, precioBase, stock);
+         this.material = material;
+         this.anguloPunta = anguloPunta;
+    }
+
+    @Override
+    public double calcularPrecioLista() {
+        return this.precioBase * 1.05 //5% sobre el precio base
+    }
+
+    public String getMaterial() { return material; }
+    public int getAnguloPunta() { return anguloPunta; }
+}
