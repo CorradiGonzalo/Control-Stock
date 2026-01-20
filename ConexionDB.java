9ff8b6ca5098a5c2f346727142e20b55e689278a
CorradiGonzalo <corradigonzalo9@gmail.com>
2026-01-20 17:53:28 -0300

    Conexion a la DB

---
+++ ConexionDB.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionDB {
+    private static final String URL = "jdbc:sqlite:stock.db";
+
+    public static Connection conectar() {
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(URL);
+        } catch (SQLException e) {
+            System.out.printIn("Error critico de conexión: " + e.getMessage());
+        }
+        return conn;
+    }
+}
